9eb5330ffd778e36f6c2534bb7d2e96e1d6532a4
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-05 17:04:17 +0300

    Added a search component to the Events Page. Reused the Button component with an option to function as either a link or a button.

--- components/common/button.js
+++ components/common/button.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
 import classes from './button.module.css';
 
-export default function Button({ link, children }) {
+export default function Button({ link, children, handleClick }) {
+    if (link) {
+        return (
+            <Link href={link} className={classes.btn}>
+                {children}
+            </Link>
+        );
+    }
+
     return (
-        <Link href={link} className={classes.btn}>
+        <button onClick={handleClick} className={classes.btn}>
             {children}
-        </Link>
+        </button>
     );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,14 @@
 import { getAllEvents } from '../../service/events.js';
 import EventList from '../../components/events/event-list.js';
+import EventsSearch from '../../components/events/events-search.js';
 
 export default function EventsPage() {
     const events = getAllEvents();
 
     return (
-        <div>
+        <>
+            <EventsSearch />
             <EventList items={events} />
-        </div>
+        </>
     );
 }
