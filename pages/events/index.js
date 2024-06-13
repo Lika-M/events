8dcbeaad3a8ae7423e4207fea2671e43c4d68abd
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-13 17:09:11 +0300

    Configured the head content with static title and metadata.

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,11 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router.js';
 
 import { getAllEvents } from '../../service/events.js';
 import EventList from '../../components/events/event-list.js';
 import EventsSearch from '../../components/events/events-search.js';
 
-export default function EventsPage({events}) {
+export default function EventsPage({ events }) {
     const router = useRouter();
 
     function searchEventHandler(year, month) {
@@ -14,18 +15,23 @@ export default function EventsPage({events}) {
 
     return (
         <>
+            <Head>
+                <title>Events</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <meta name="description" content="Find a lot of great events that alow you to evolve."></meta>
+            </Head>
             <EventsSearch onSearch={searchEventHandler} />
             <EventList items={events} />
         </>
     );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
     const allEvents = getAllEvents();
 
     return {
         props: {
-           events: allEvents
+            events: allEvents
         }
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
-// import { getFeaturedEvents } from '../service/events.js';
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../api/api-util.js';
 import EventList from '../components/events/event-list.js';
 
-export default function HomePage({events}) {
+export default function HomePage({ events }) {
     return (
-        <div>
-            <EventList items={events}/>
-        </div>
-    )
+        <>
+            <Head>
+                <title>New Events</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <meta name="description" content="Find a lot of great events that alow you to evolve."></meta>
+            </Head>
+            <div>
+                <EventList items={events} />
+            </div>
+        </>
+    );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
     const featuredEvents = await getFeaturedEvents();
 
     return {
         props: {
             events: featuredEvents
         },
-        //at most once every 10 min
         revalidate: 600
-    }
+    };
 }
