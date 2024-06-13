9d926ea42db92f1cb0040c9e69be8065b320f62e
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-13 19:22:06 +0300

    Configured the head content metadata in the root app component to apply generic settings across all page components.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+import Head from 'next/head';
 import Layout from '../components/layout/layout.js'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
-  )}
+  );
+}
 
 export default MyApp;
