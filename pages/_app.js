408482c42d2804e9785cacc469c497070821ed68
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-13 19:40:45 +0300

    Added general head metadata in the _app component, which is overwritten or merged in nested components.

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
