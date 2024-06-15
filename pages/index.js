1c04d94f9a37fc3a6efa8c63fa5008179c95c278
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-15 12:27:38 +0300

    Added UI component for newsletter subscription registration.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 import { getFeaturedEvents } from '../api/api-util.js';
 import EventList from '../components/events/event-list.js';
+import NewsletterRegistration from '../components/registration/newsletter-registration.js';
 
 export default function HomePage({ events }) {
     return (
@@ -11,6 +12,7 @@ export default function HomePage({ events }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
                 <meta name="description" content="Find a lot of great events that alow you to evolve."></meta>
             </Head>
+            <NewsletterRegistration />
             <div>
                 <EventList items={events} />
             </div>
