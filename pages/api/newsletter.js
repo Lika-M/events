02bcc22b03393ef568164b1b432a1b7d17687251
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-16 16:20:35 +0300

    Set up Mongo Atlas DB 'events' and added collection 'newsletter' for storing emails

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,31 +1,49 @@
-import fs from 'fs';
-import path from 'path';
-import { cwd } from 'process';
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-export default function handler(req, res) {
+const uri = 'mongodb+srv://Lika-M:t0cDyE1rfD2w1U20@cluster0.wnmsq4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+
+
+
+export default async function handler(req, res) {
+    console.log("Request received");
+
     if (req.method === 'POST') {
+    
         const email = req.body.email;
 
         if (!email || !email.includes('@') || !email.includes('.')) {
-            return res.status(422).json({ message: 'Invalid email.address.' })
+            console.log("Invalid email address:", email);
+            return res.status(422).json({ message: 'Invalid email address.' });
         }
 
-        const userData = {
-            id: (Math.random() * 1000000).toFixed(0),
-            email: email
+        const client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+
+        try {
+            await client.connect();
+
+            const db = client.db('events'); 
+            const collection = db.collection('newsletter');
+            
+            const result = await collection.insertOne({ email: email });
+            res.status(201).json({ email: email, message: 'Signed up.', result: result });
+
+        } catch (error) {
+           
+            res.status(500).json({ message: 'Storing email failed!' });
+        } finally {
+
+            await client.close();
         }
-
-        const filePath = path.join(cwd(), 'data', 'feedback.json');
-        const storedData = fs.readFileSync(filePath);
-
-        const data = JSON.parse(storedData);
-        data.push(userData);
-
-        fs.writeFileSync(filePath, JSON.stringify(data));
-
-        res.status(201).json({ email: email, message: 'Signed up.' });
-
     } else {
-
+       
+        res.status(405).json({ message: 'We only support POST requests.' });
     }
 }
+
