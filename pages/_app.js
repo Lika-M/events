004e05276556d93fca69c7989c0f25655a48da83
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-20 16:14:03 +0300

    Added Notification component and styles to the main layout.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '../components/layout/layout.js'
 import '../styles/globals.css'
+import Notification from '../components/notification/notification.js';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
+      <Notification title='Test' message= 'This is test' status = 'error'/>
     </Layout>
   );
 }
